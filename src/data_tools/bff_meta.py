39c212349f79c70ec6a97e68dfcfb4f71bf5faf7
rymuelle <rmueller@physics.tamu.edu>
2022-12-30 19:31:21 +0100

    refactored combine df imports

---
+++ src/data_tools/bff_meta.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+nJets = [1,2]
+regions = ['SR{}','CR{}0','CR{}3','CR{}4']
+weights = ['Weight_Pu{}',
+ 'Weight_BTag{}',
+ 'Weight_PUID{}',
+ 'Weight_PDF_ISRFSR_{}',
+ 'Weight_MuonSF{}',
+ 'Weight_ElectronSF{}']
+jet_variations = ['jer{}', 'jesTotal{}']
+var_types = ['Up','Down']
+all_reg = [reg.format(nJet) for reg in regions for nJet in nJets]
+weights = [[weight.format(v) for v in var_types] for weight in weights]
+jet_variations = [[jv.format(v) for v in var_types] for jv in jet_variations]
+
+BFF_cut_values ={1:{'HTLT':-18,'RelMET':.3125,'SBM':18.75}, 
+                    2:{'HTLT':-10,'RelMET':.2625,'SBM':1.25}}
+
+def bff_cuts_pd(df, reg, jv, bff_cv=BFF_cut_values):
+    import re
+    import pandas as pd
+    nJets = int(re.findall('(?:SR|CR)([0-9])', reg)[0])
+    _bff_cv = bff_cv[nJets]
+    less_than, greater_than, equal_to = {},{},{}
+    less_than['HTLT_'+jv] = _bff_cv['HTLT']
+    less_than['RelMET_'+jv] = _bff_cv['RelMET']
+    less_than['DiLepMass'] = 800
+    greater_than['SBM_'+jv] = _bff_cv['SBM']
+    greater_than['DiLepMass'] = 105
+    equal_to[reg+'_'+jv] = 1
+    equal_to['Flag_METFilters'] = 1
+    return df.loc[(df[list(less_than)] < pd.Series(less_than)).all(axis=1) & 
+                 (df[list(greater_than)] > pd.Series(greater_than)).all(axis=1) & 
+                 (df[list(equal_to)] == pd.Series(equal_to)).all(axis=1)
+                 ]
+def band_cut(column, low=-np.inf,high=np.inf):
+    import pandas as pd
+    def cut_df(df):
+        return df[(df[column] > low) & ( df[column]< high)]
+    return cut_df
+
+def band_cut2d(column1, column2, low=[0,-np.inf], high=[0,np.inf]):
+    import pandas as pd 
+    from bff_processor.utils import linear
+    def cut_df(df):
+        return df[(linear(df[column1],*low) < df[column2]) & (linear(df[column1],*high) > df[column2])]
+    return cut_df    
+
+def isin(string):
+    import pandas as pd
+    def cut_df(df):
+        return df[df[string]==1]
+    return cut_df
+
+def preselection(dr=0.4):
+    import pandas as pd
+    import numpy as np
+    def cut_df(df):
+        df['deltaR'] = df[["minGoodJetElDR","minGoodJetMuDR"]].min(axis=1)
+        df = band_cut('deltaR',low=dr,high=np.inf)(df)
+        return isin('Flag_METFilters')(band_cut('DiLepMass',low=105,high=800)(df))
+    return cut_df
+
+#identity wrapper for df
+def identity(*args, **kwargs):
+    def wrapper(x):
+        return x
+    return wrapper
+
+
+def make_bff_cuts():
+    import yaml
+    with open('../bff_cut.yml', 'r') as f:
+        param_values = yaml.load(f, yaml.SafeLoader)
+    popt_relmet_1 = param_values['popt_relmet_1']
+    popt_relmet_2 = param_values['popt_relmet_2']
+    RelMET_ff1 = lambda x: band_cut2d('DiLepMass','RelMET_nom', high=popt_relmet_1)(isin('SR1_nom')(x))
+    RelMET_ff2 = lambda x: band_cut2d('DiLepMass','RelMET_nom', high=popt_relmet_2)(isin('SR2_nom')(x))
+    
+    popt_htlt_1 = param_values['popt_htlt_1']
+    popt_htlt_2 = param_values['popt_htlt_2']
+    HTLT_ff1 = lambda x: band_cut2d('DiLepMass','HTLT_nom', high=popt_htlt_1)(RelMET_ff1(x))
+    HTLT_ff2 = lambda x: band_cut2d('DiLepMass','HTLT_nom', high=popt_htlt_2)(RelMET_ff2(x))
+    
+    popt_TMB_1 = param_values['popt_TMB_1']
+    popt_TMB_2 = param_values['popt_TMB_2']
+    TMB_ff1 = lambda x: band_cut2d('DiLepMass','TMB_nom', low=popt_TMB_1)(HTLT_ff1(x))
+    TMB_ff2 = lambda x: band_cut2d('DiLepMass','TMB_nom', low=popt_TMB_2)(HTLT_ff2(x))
+    
---
+++ src/data_tools/filename_skim_tools.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import os
+import re
+
+def regex_select(era):
+    return "tw_{}.+\.csv".format(era)
+
+def get_files(regex_select, path='data'):
+    files = ["{}/{}".format(path,x) for x in os.listdir(path) if re.match(regex_select, x)]
+    DY = [x for x in files if re.match('.+ZTo(?:Mu|EE).+', x)]
+    ST = [x for x in files if re.match('.+top.csv', x)]
+    VB = [x for x in files if re.match('.+mc_(?:ww|wz|zz)', x)]
+    TT = [x for x in files if re.match('.+ttbar', x)]
+    BFF = [x for x in files if re.match('.+BFFZp', x)]
+    data = [x for x in files if re.match('.+_data_', x)]
+    assert len(files) == len(DY+ST+TT+VB+data+BFF), "duplicate or uncaught file"
+    return {'DY': DY,'ST': ST,'VB': VB,'TT': TT,'BFF': BFF,'data': data}
+
+def make_df(df_list, verbose=0):
+    t_df = pd.DataFrame()
+    for fname in df_list:
+        if verbose: print(fname)
+        t_df = pd.concat([t_df, pd.read_csv(fname)])
+        t_df.replace([np.inf, -np.inf], np.nan, inplace=True)
+        t_df.dropna(subset =['Weight'],inplace=True)
+    return t_df
