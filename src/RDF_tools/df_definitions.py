5a6a1bff9785e1f598f744bf4ab1cc3789a35655
rymuelle <rmueller@physics.tamu.edu>
2023-02-22 22:28:16 +0100

    updated for corr and uncorr btag

--- src/RDF_tools/df_definitions.py
+++ src/RDF_tools/df_definitions.py
@@ -7,20 +7,35 @@ from glob import glob
 def bjet_weight(df,ismc, is_inclusive, name,sample_weight, era, var_string='_jet_nom_muon_corrected_pt_ele_pt'):
     if int(ismc):
         df = df.Define("BTagWeight", "map_zero_to_one(GetBTagWeight(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M))".format(var_string, var_string))
-        df = df.Define("BTagWeightUp", "map_zero_to_one(GetBTagWeight(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_up))".format(var_string, var_string))
-        df = df.Define("BTagWeightDown", "map_zero_to_one(GetBTagWeight(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_down))".format(var_string, var_string)) 
+        
+        #correlated
+        df = df.Define("BTagWeightCorrUp", "map_zero_to_one(GetBTagWeight(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_up_correlated))".format(var_string, var_string))
+        df = df.Define("BTagWeightCorrDown", "map_zero_to_one(GetBTagWeight(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_down_correlated))".format(var_string, var_string)) 
         #compute per jet sf
-        df = df.Define("BTagWeightUpPerEvent", "GetBTagWeightPerJet(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_up)".format(var_string, var_string))
-        df = df.Define("BTagWeightPerEvent", "GetBTagWeightPerJet(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M)".format(var_string, var_string))
-        df = df.Define('AvgBtagWeight', 'CalcAverage(DivideVec(BTagWeightUpPerEvent,BTagWeightPerEvent))')
+        df = df.Define("BTagWeightCorrUpPerEvent", "GetBTagWeightPerJet(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_up_correlated)".format(var_string, var_string))
+        df = df.Define("BTagWeightCorrPerEvent", "GetBTagWeightPerJet(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M)".format(var_string, var_string))
+        df = df.Define('AvgBtagWeightCorr', 'CalcAverage(DivideVec(BTagWeightCorrUpPerEvent,BTagWeightCorrPerEvent))')
+        
+        #uncorrelated
+        df = df.Define("BTagWeightUncorrUp", "map_zero_to_one(GetBTagWeight(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_up_uncorrelated))".format(var_string, var_string))
+        df = df.Define("BTagWeightUncorrDown", "map_zero_to_one(GetBTagWeight(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_down_uncorrelated))".format(var_string, var_string)) 
+        #compute per jet sf
+        df = df.Define("BTagWeightUncorrUpPerEvent", "GetBTagWeightPerJet(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_up_uncorrelated)".format(var_string, var_string))
+        df = df.Define("BTagWeightUncorrPerEvent", "GetBTagWeightPerJet(GoodBJet{}, GoodJet{}, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M)".format(var_string, var_string))
+        df = df.Define('AvgBtagWeightUncorr', 'CalcAverage(DivideVec(BTagWeightUncorrUpPerEvent,BTagWeightUncorrPerEvent))')
     else:
         df = df.Define("BTagWeight", "1.")
-        df = df.Define("BTagWeightUp", "1.")
-        df = df.Define("BTagWeightDown", "1.")
+        df = df.Define("BTagWeightCorrUp", "1.")
+        df = df.Define("BTagWeightCorrDown", "1.")
+        df = df.Define("BTagWeightUncorrUp", "1.")
+        df = df.Define("BTagWeightUncorrDown", "1.")
         #compute per jet sf
-        df = df.Define("BTagWeightUpPerEvent", "1.")
-        df = df.Define("BTagWeightPerEvent", "1.")
-        df = df.Define('AvgBtagWeight', "1.")
+        df = df.Define("BTagWeightCorrUpPerEvent", "1.")
+        df = df.Define("BTagWeightCorrPerEvent", "1.")
+        df = df.Define('AvgBtagWeightCorr', "1.")
+        df = df.Define("BTagWeightUncorrUpPerEvent", "1.")
+        df = df.Define("BTagWeightUncorrPerEvent", "1.")
+        df = df.Define('AvgBtagWeightUncorr', "1.")
     return df
 
 
@@ -143,8 +158,10 @@ def finalize_weights(df,ismc, is_inclusive, name,sample_weight, era):
         # Systematic weights
         df = df.Define("Weight_PuUp", "Weight/puWeight*puWeightUp")
         df = df.Define("Weight_PuDown", "Weight/puWeight*puWeightDown")
-        df = df.Define("Weight_BTagUp", "Weight/BTagWeight*BTagWeightUp")
-        df = df.Define("Weight_BTagDown", "Weight/BTagWeight*BTagWeightDown")
+        df = df.Define("Weight_BTagCorrUp", "Weight/BTagWeight*BTagWeightCorrUp")
+        df = df.Define("Weight_BTagCorrDown", "Weight/BTagWeight*BTagWeightCorrDown")
+        df = df.Define("Weight_BTagUncorrUp", "Weight/BTagWeight*BTagWeightUncorrUp")
+        df = df.Define("Weight_BTagUncorrDown", "Weight/BTagWeight*BTagWeightUncorrDown")
         df = df.Define("Weight_PUIDUp", "PUIDWeightUp/PUIDWeight*Weight")
         df = df.Define("Weight_PUIDDown", "PUIDWeightDown/PUIDWeight*Weight")
         df = df.Define("Weight_ISRFSR_Up", "Weight*fsr_isr_weightUp")
@@ -308,105 +325,105 @@ def def_HLT(df, ismc, era):
     return df
 
 
-def def_sf_and_weight(df,ismc, is_inclusive, name,sample_weight, era):
-    if int(ismc):
-        #if name not in ['mc_zz', 'mc_wz','mc_ww']:
-        #    df = df.Define("PDF_ISRFSR_uncertainty","GetPDFandScaleUncertainty(LHEPdfWeight, LHEScaleWeight)")
-        #    df = df.Define("PDF_ISRFSR_uncertaintyUp","PDF_ISRFSR_uncertainty[0]")
-        #    df = df.Define("PDF_ISRFSR_uncertaintyDown","PDF_ISRFSR_uncertainty[1]")
-        #else:
-        #    df = df.Define("PDF_ISRFSR_uncertainty","1")
-        #    df = df.Define("PDF_ISRFSR_uncertaintyUp","1")
-        #    df = df.Define("PDF_ISRFSR_uncertaintyDown","1")
-        ##correct incorrect muon GoodMuon_effSF_stat_trigger value
-        #df = df.Define('GoodMuon_effSF_stat_trigger_corr', 'find_replace(GoodMuon_effSF_stat_trigger, 1., .0)')
-        #
-        #
-        #
-        #
-        #df = df.Define("MuonSFweights","CalculateMuonScaleFactor(GoodMuon_effSF_trigger, GoodMuon_effSF_stat_trigger_corr, GoodMuon_effSF_ID, GoodMuon_effSF_sys_ID, GoodMuon_effSF_stat_ID, GoodMuon_effSF_ISO, GoodMuon_effSF_sys_ISO, GoodMuon_effSF_stat_ISO)")
-        #df = df.Define("MuonSFweight","MuonSFweights[0]")
-        #df = df.Define("MuonSFweightUp","MuonSFweights[1]")
-        #df = df.Define("MuonSFweightDown","MuonSFweights[2]")
-        #df = df.Define("ElectronSFweights","CalculateElectronScaleFactor(GoodElectron_effSF, GoodElectron_effSF_stat, GoodElectron_effSF_sys)")
-        #df = df.Define("ElectronSFweight","map_zero_to_one(ElectronSFweights[0])")
-        #df = df.Define("ElectronSFweightUp","map_zero_to_one(ElectronSFweights[1])")
-        #df = df.Define("ElectronSFweightDown","map_zero_to_one(ElectronSFweights[2])")
-        #if "ZTo" in name: 
-        #    df = df.Define("k_factor","k_factor(2016, DiLepMass)")
-        #else: df = df.Define("k_factor","1")
-        if not is_inclusive:
-            #df = df.Define("PUIDWeights", "GetPUIDweight(NoPUID_GoodJetPt, NoPUID_GoodJetEta, NoPUID_GoodJetGenJetIdx, NoPUID_PUID)")
-            #df = df.Define("PUIDWeight", "map_zero_to_one(PUIDWeights[0])")
-            #df = df.Define("PUIDWeightUp", "map_zero_to_one(PUIDWeights[1])")
-            #df = df.Define("PUIDWeightDown", "map_zero_to_one(PUIDWeights[2])")
-            #df = df.Define("GoodJetBTagSF", "(Jet_btagSF_deepflavour_M[GoodJet])")
-            #df = df.Define("GoodJetBTagSFUp", "(Jet_btagSF_deepflavour_M_up[GoodJet])")
-            #df = df.Define("GoodJetBTagSFDown", "(Jet_btagSF_deepflavour_M_down[GoodJet])")
-            #df = df.Define("GoodJetHadronFlav","Jet_hadronFlavour[GoodJet]")
-            df = df.Define("BTagWeight", "map_zero_to_one(GetBTagWeight(GoodBJet, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M))")
-            df = df.Define("BTagWeightUp", "map_zero_to_one(GetBTagWeight(GoodBJet, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_up))")
-            df = df.Define("BTagWeightDown", "map_zero_to_one(GetBTagWeight(GoodBJet, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_down))")
-            
-
-            ##event weights
-            #df = df.Define("sample_weight", str(sample_weight))
-            #if era=="2016" or era=="2017":
-            #    weight_string = "{}*copysign(1.,genWeight)*k_factor*puWeight*PUIDWeight*MuonSFweight*ElectronSFweight*TriggerWeight*L1PreFiringWeight_Nom".format(sample_weight)
-            #else: 
-            #    weight_string = "{}*copysign(1.,genWeight)*k_factor*puWeight*BTagWeight*PUIDWeight*MuonSFweight*ElectronSFweight*TriggerWeight".format(sample_weight)
-            #df = df.Define("Weight", weight_string)
-            ## Systematic weights
-            #df = df.Define("Weight_PuUp", "Weight/puWeight*puWeightUp")
-            #df = df.Define("Weight_PuDown", "Weight/puWeight*puWeightDown")
-            #df = df.Define("Weight_BTagUp", "Weight/BTagWeight*BTagWeightUp")
-            #df = df.Define("Weight_BTagDown", "Weight/BTagWeight*BTagWeightDown")
-            #df = df.Define("Weight_PUIDUp", "Weight/PUIDWeight*PUIDWeightUp")
-            #df = df.Define("Weight_PUIDDown", "Weight/PUIDWeight*PUIDWeightDown")
-            #df = df.Define("Weight_PDF_ISRFSR_Up", "Weight*PDF_ISRFSR_uncertaintyUp")
-            #df = df.Define("Weight_PDF_ISRFSR_Down", "Weight*PDF_ISRFSR_uncertaintyDown")
-            #df = df.Define("Weight_MuonSFUp", "Weight/MuonSFweight*MuonSFweightUp")
-            #df = df.Define("Weight_MuonSFDown", "Weight/MuonSFweight*MuonSFweightDown")
-            #df = df.Define("Weight_ElectronSFUp", "Weight/ElectronSFweight*ElectronSFweightUp")
-            #df = df.Define("Weight_ElectronSFDown", "Weight/ElectronSFweight*ElectronSFweightDown")
-            #if era=="2016" or era=="2017":
-            #    df = df.Define("Weight_L1Down", "Weight/L1PreFiringWeight_Nom*L1PreFiringWeight_Dn")
-            #    df = df.Define("Weight_L1Up", "Weight/L1PreFiringWeight_Nom*L1PreFiringWeight_Up")
-            #else:
-            #    "nothing here"
-            #    #these are not required, and contain misleading values
-            #    #df = df.Define("Weight_jesHEMUp", "Weight*JetSFWeight_jesHEMIssueUp")
-            #    #df = df.Define("Weight_jesHEMDown", "Weight*JetSFWeight_jesHEMIssueDown")
-        #else:  #Inclusive sample doesn't cut on btags, so no btag weight needed; also no PUID needed
-            #df = df.Define("Weight", "{}*copysign(1.,genWeight)*k_factor*puWeight*MuonSFweight*ElectronSFweight*TriggerWeight".format(sample_weight))
-            #df = df.Define("sample_weight", "1")
-            #df = df.Define("Weight_PuUp", "Weight/puWeight*puWeightUp")
-            #df = df.Define("Weight_PuDown", "Weight/puWeight*puWeightDown")
-            #df = df.Define("Weight_PDF_ISRFSR_Up", "Weight*PDF_ISRFSR_uncertaintyUp")
-            #df = df.Define("Weight_PDF_ISRFSR_Down", "Weight*PDF_ISRFSR_uncertaintyDown")
-            #df = df.Define("Weight_MuonSFUp", "Weight/MuonSFweight*MuonSFweightUp")
-            #df = df.Define("Weight_MuonSFDown", "Weight/MuonSFweight*MuonSFweightDown")
-            #df = df.Define("Weight_ElectronSFUp", "Weight/ElectronSFweight*ElectronSFweightUp")
-            #df = df.Define("Weight_ElectronSFDown", "Weight/ElectronSFweight*ElectronSFweightDown")
-    else:
-        print("genWeight")
-        df = df.Define("Weight", "1.")
-        df = df.Define("genWeight", "1.")
-        df = df.Define("sample_weight", "1")
-        df = df.Define("Weight_PuDown", "1")
-        df = df.Define("Weight_PuUp", "1")
-        df = df.Define("Weight_BTagUp", "1")
-        df = df.Define("Weight_BTagDown", "1")
-        df = df.Define("Weight_PUIDUp", "1")
-        df = df.Define("Weight_PUIDDown", "1")
-        df = df.Define("Weight_PDF_ISRFSR_Up", "1")
-        df = df.Define("Weight_PDF_ISRFSR_Down", "1")
-        df = df.Define("Weight_MuonSFUp", "1")
-        df = df.Define("Weight_MuonSFDown", "1")
-        df = df.Define("Weight_ElectronSFUp", "1")
-        df = df.Define("Weight_ElectronSFDown", "1")
-        df = df.Define("k_factor","1")  
-    return df
+#def def_sf_and_weight(df,ismc, is_inclusive, name,sample_weight, era):
+#    if int(ismc):
+#        #if name not in ['mc_zz', 'mc_wz','mc_ww']:
+#        #    df = df.Define("PDF_ISRFSR_uncertainty","GetPDFandScaleUncertainty(LHEPdfWeight, LHEScaleWeight)")
+#        #    df = df.Define("PDF_ISRFSR_uncertaintyUp","PDF_ISRFSR_uncertainty[0]")
+#        #    df = df.Define("PDF_ISRFSR_uncertaintyDown","PDF_ISRFSR_uncertainty[1]")
+#        #else:
+#        #    df = df.Define("PDF_ISRFSR_uncertainty","1")
+#        #    df = df.Define("PDF_ISRFSR_uncertaintyUp","1")
+#        #    df = df.Define("PDF_ISRFSR_uncertaintyDown","1")
+#        ##correct incorrect muon GoodMuon_effSF_stat_trigger value
+#        #df = df.Define('GoodMuon_effSF_stat_trigger_corr', 'find_replace(GoodMuon_effSF_stat_trigger, 1., .0)')
+#        #
+#        #
+#        #
+#        #
+#        #df = df.Define("MuonSFweights","CalculateMuonScaleFactor(GoodMuon_effSF_trigger, GoodMuon_effSF_stat_trigger_corr, GoodMuon_effSF_ID, GoodMuon_effSF_sys_ID, GoodMuon_effSF_stat_ID, GoodMuon_effSF_ISO, GoodMuon_effSF_sys_ISO, GoodMuon_effSF_stat_ISO)")
+#        #df = df.Define("MuonSFweight","MuonSFweights[0]")
+#        #df = df.Define("MuonSFweightUp","MuonSFweights[1]")
+#        #df = df.Define("MuonSFweightDown","MuonSFweights[2]")
+#        #df = df.Define("ElectronSFweights","CalculateElectronScaleFactor(GoodElectron_effSF, GoodElectron_effSF_stat, GoodElectron_effSF_sys)")
+#        #df = df.Define("ElectronSFweight","map_zero_to_one(ElectronSFweights[0])")
+#        #df = df.Define("ElectronSFweightUp","map_zero_to_one(ElectronSFweights[1])")
+#        #df = df.Define("ElectronSFweightDown","map_zero_to_one(ElectronSFweights[2])")
+#        #if "ZTo" in name: 
+#        #    df = df.Define("k_factor","k_factor(2016, DiLepMass)")
+#        #else: df = df.Define("k_factor","1")
+#        if not is_inclusive:
+#            #df = df.Define("PUIDWeights", "GetPUIDweight(NoPUID_GoodJetPt, NoPUID_GoodJetEta, NoPUID_GoodJetGenJetIdx, NoPUID_PUID)")
+#            #df = df.Define("PUIDWeight", "map_zero_to_one(PUIDWeights[0])")
+#            #df = df.Define("PUIDWeightUp", "map_zero_to_one(PUIDWeights[1])")
+#            #df = df.Define("PUIDWeightDown", "map_zero_to_one(PUIDWeights[2])")
+#            #df = df.Define("GoodJetBTagSF", "(Jet_btagSF_deepflavour_M[GoodJet])")
+#            #df = df.Define("GoodJetBTagSFUp", "(Jet_btagSF_deepflavour_M_up[GoodJet])")
+#            #df = df.Define("GoodJetBTagSFDown", "(Jet_btagSF_deepflavour_M_down[GoodJet])")
+#            #df = df.Define("GoodJetHadronFlav","Jet_hadronFlavour[GoodJet]")
+#            df = df.Define("BTagWeight", "map_zero_to_one(GetBTagWeight(GoodBJet, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M))")
+#            df = df.Define("BTagWeightUp", "map_zero_to_one(GetBTagWeight(GoodBJet, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_up))")
+#            df = df.Define("BTagWeightDown", "map_zero_to_one(GetBTagWeight(GoodBJet, Jet_hadronFlavour, Jet_pt, Jet_btagSF_deepflavour_M_down))")
+#            
+#
+#            ##event weights
+#            #df = df.Define("sample_weight", str(sample_weight))
+#            #if era=="2016" or era=="2017":
+#            #    weight_string = "{}*copysign(1.,genWeight)*k_factor*puWeight*PUIDWeight*MuonSFweight*ElectronSFweight*TriggerWeight*L1PreFiringWeight_Nom".format(sample_weight)
+#            #else: 
+#            #    weight_string = "{}*copysign(1.,genWeight)*k_factor*puWeight*BTagWeight*PUIDWeight*MuonSFweight*ElectronSFweight*TriggerWeight".format(sample_weight)
+#            #df = df.Define("Weight", weight_string)
+#            ## Systematic weights
+#            #df = df.Define("Weight_PuUp", "Weight/puWeight*puWeightUp")
+#            #df = df.Define("Weight_PuDown", "Weight/puWeight*puWeightDown")
+#            #df = df.Define("Weight_BTagUp", "Weight/BTagWeight*BTagWeightUp")
+#            #df = df.Define("Weight_BTagDown", "Weight/BTagWeight*BTagWeightDown")
+#            #df = df.Define("Weight_PUIDUp", "Weight/PUIDWeight*PUIDWeightUp")
+#            #df = df.Define("Weight_PUIDDown", "Weight/PUIDWeight*PUIDWeightDown")
+#            #df = df.Define("Weight_PDF_ISRFSR_Up", "Weight*PDF_ISRFSR_uncertaintyUp")
+#            #df = df.Define("Weight_PDF_ISRFSR_Down", "Weight*PDF_ISRFSR_uncertaintyDown")
+#            #df = df.Define("Weight_MuonSFUp", "Weight/MuonSFweight*MuonSFweightUp")
+#            #df = df.Define("Weight_MuonSFDown", "Weight/MuonSFweight*MuonSFweightDown")
+#            #df = df.Define("Weight_ElectronSFUp", "Weight/ElectronSFweight*ElectronSFweightUp")
+#            #df = df.Define("Weight_ElectronSFDown", "Weight/ElectronSFweight*ElectronSFweightDown")
+#            #if era=="2016" or era=="2017":
+#            #    df = df.Define("Weight_L1Down", "Weight/L1PreFiringWeight_Nom*L1PreFiringWeight_Dn")
+#            #    df = df.Define("Weight_L1Up", "Weight/L1PreFiringWeight_Nom*L1PreFiringWeight_Up")
+#            #else:
+#            #    "nothing here"
+#            #    #these are not required, and contain misleading values
+#            #    #df = df.Define("Weight_jesHEMUp", "Weight*JetSFWeight_jesHEMIssueUp")
+#            #    #df = df.Define("Weight_jesHEMDown", "Weight*JetSFWeight_jesHEMIssueDown")
+#        #else:  #Inclusive sample doesn't cut on btags, so no btag weight needed; also no PUID needed
+#            #df = df.Define("Weight", "{}*copysign(1.,genWeight)*k_factor*puWeight*MuonSFweight*ElectronSFweight*TriggerWeight".format(sample_weight))
+#            #df = df.Define("sample_weight", "1")
+#            #df = df.Define("Weight_PuUp", "Weight/puWeight*puWeightUp")
+#            #df = df.Define("Weight_PuDown", "Weight/puWeight*puWeightDown")
+#            #df = df.Define("Weight_PDF_ISRFSR_Up", "Weight*PDF_ISRFSR_uncertaintyUp")
+#            #df = df.Define("Weight_PDF_ISRFSR_Down", "Weight*PDF_ISRFSR_uncertaintyDown")
+#            #df = df.Define("Weight_MuonSFUp", "Weight/MuonSFweight*MuonSFweightUp")
+#            #df = df.Define("Weight_MuonSFDown", "Weight/MuonSFweight*MuonSFweightDown")
+#            #df = df.Define("Weight_ElectronSFUp", "Weight/ElectronSFweight*ElectronSFweightUp")
+#            #df = df.Define("Weight_ElectronSFDown", "Weight/ElectronSFweight*ElectronSFweightDown")
+#    else:
+#        print("genWeight")
+#        df = df.Define("Weight", "1.")
+#        df = df.Define("genWeight", "1.")
+#        df = df.Define("sample_weight", "1")
+#        df = df.Define("Weight_PuDown", "1")
+#        df = df.Define("Weight_PuUp", "1")
+#        df = df.Define("Weight_BTagUp", "1")
+#        df = df.Define("Weight_BTagDown", "1")
+#        df = df.Define("Weight_PUIDUp", "1")
+#        df = df.Define("Weight_PUIDDown", "1")
+#        df = df.Define("Weight_PDF_ISRFSR_Up", "1")
+#        df = df.Define("Weight_PDF_ISRFSR_Down", "1")
+#        df = df.Define("Weight_MuonSFUp", "1")
+#        df = df.Define("Weight_MuonSFDown", "1")
+#        df = df.Define("Weight_ElectronSFUp", "1")
+#        df = df.Define("Weight_ElectronSFDown", "1")
+#        df = df.Define("k_factor","1")  
+#    return df
 
 def def_lep_selections(df):
     df = df.Define('nLep', 'nEle+nMuons')
