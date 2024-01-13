2e1a5047fd36708a27df5cfab2db35955aa59079
rymuelle <rmueller@physics.tamu.edu>
2024-01-13 21:55:42 +0100

    attempting to add some new features to 5_interp_signal_model_ind_makedf-indp_bins.ipynb

---
+++ src/assets/shape_scaling.py
@@ -0,0 +1,4 @@
+scale = 5000
+#this controlls the scaling up of the shape histograms for better performance in higgs combine
+
+#e.g. 5_interp_signal_model_ind_makedf-indp_bins and 7_make_comb_csv_bffv2-model_ind
